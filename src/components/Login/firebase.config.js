97c720499f6875c4d604502899d48bf995ae821e
koushik16101085 <16101085@uap-bd.edu>
2021-03-28 14:20:47 +0600

    Final Update Commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZiMD3pRB5-4I00QNV8odaEziPnNmfadk",
+  authDomain: "burj-ai-arab.firebaseapp.com",
+  projectId: "burj-ai-arab",
+  storageBucket: "burj-ai-arab.appspot.com",
+  messagingSenderId: "1033702218932",
+  appId: "1:1033702218932:web:65d8686666c93e144bcbe1"
+};
